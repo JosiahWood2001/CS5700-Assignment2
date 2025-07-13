c4b95ea6fa8cf5041bdc5bc4da84605046e52fc6
Josiah Wood <A02298566@aggies.usu.edu>
2025-07-13 03:32:13 -0600

    Created simple framework based on class demonstration

---
+++ ShipmentTrackingSimulator/composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,33 @@
+package org.example.project
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material.Divider
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import shipmenttrackingsimulator.composeapp.generated.resources.Res
+import shipmenttrackingsimulator.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Column {
+            Divider(Modifier.height(5.dp))
+            ShipmentSubscriptions()
+        }
+    }
+}
---
+++ ShipmentTrackingSimulator/composeApp/src/desktopMain/kotlin/org/example/project/ShipmentSubscriptions.kt
@@ -0,0 +1,34 @@
+package org.example.project
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun ShipmentSubscriptions(){
+    val shipments = remember{ mutableStateListOf<Shipment>()}
+    var searchField by remember{ mutableStateOf("")}
+    Row{
+        TextField(searchField, onValueChange={searchField=it})
+        Button(onClick = {
+            shipments += Shipment()
+            searchField = ""
+        }){
+            Text("Search")
+        }
+    }
+    LazyColumn{
+        items(shipments){
+            Text("this")
+        }
+    }
+}
